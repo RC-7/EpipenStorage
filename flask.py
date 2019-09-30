0640daddb0a2a268bbfda93113bcf43208d87e8e
RC-7 <rashcassim@gmail.com>
2019-09-30 11:38:43 +0200

    Began adding second paper's model

--- flask.py
+++ flask.py
@@ -4,16 +4,19 @@ from temperatureModel import tempModel
 class flask():
     def __init__(self,modelNumber):
         self.tempInner=8            #Set initial temperature to middle of range we want
-        self.radius=2              #initial estimate, 1.2 for eppi pen, rest for peltier and other wall
+        self.radius=2/100              #initial estimate, 1.2 for eppi pen, rest for peltier and other wall
         self.wallThickness=0.1
-        self.length=16              #has a margin, need 14.8 only
+        self.length=16/100              #has a margin, need 14.8 only
         self.areaOuter=2*math.pi*self.radius*self.length+2*math.pi*self.radius**2            #has top and bottom
         self.areainner=2*math.pi*self.radius*self.length+2*math.pi*self.radius**2            #change to be inner area and below
         self.volumeInner=math.pi*self.radius**2*self.length
         self.model=tempModel(modelNumber)
         self.density=1.1839                     #Make depd on temperature, units kg/m^3
         self.specificHeat=1.005                 #Make depd on temperature,
-        self.k={0:24.36,
+
+        self.outfile="Model"+str(modelNumber)+".txt"
+
+        self.k={0:24.36,            #Assumes this is temp range inside will be opperating at
         5:24.74,
         10:25.12,
         15:25.50,
@@ -23,18 +26,23 @@ class flask():
         40:27.35,
         45:27.5,
         50:28.08}
-        self.U=(0.01*self.k[int(self.tempInner/5)*5])/self.radius
+
+        self.U=(0.001*self.k[int(self.tempInner/5)*5])/self.radius              #Made 1-100th
 
 
     def updateOverallCoefficient(self):
-        self.U=(0.01*self.k[int(self.tempInner/5)*5]*10**(-3))/self.radius
+        self.U=(0.001*self.k[int(self.tempInner/5)*5]*10**(-3))/self.radius
 
 
 
 
     def updateTemp(self,ambient):              
         self.updateOverallCoefficient()
-        print((self.volumeInner*self.density)/(100**3))
-        self.tempInner=self.model.newTemp([self.tempInner,ambient, self.U,self.areainner,(self.volumeInner*self.density)/(100**3),self.specificHeat ])
+        # print((self.volumeInner*self.density)/(100**3))
+        self.tempInner=self.model.newTemp([self.tempInner,ambient, self.U,self.areainner,(self.volumeInner*self.density),self.specificHeat ])
+        
+        # with open(self.outfile,'a') as f:                               #should maybe write in chunks? If slow do that with threading
+        #     f.write(str(self.tempInner)+","+str(ambient)+"\n")
+
 
        
--- temperatureModel.py
+++ temperatureModel.py
@@ -7,12 +7,57 @@ class tempModel():
 
         self.modelMethod = {1:self.paperModel,2:self.ownModel,3:self.simplifiedPaper}
 
+        
+        self.convectionCoeff=10
+        self.specificHeat=1.005  
+        self.lambdaVals = self.initLambda()
+                   #Make depd on temperature,
+
+
+
+
+    # [self.tempInner,ambient, self.U,self.areainner,(self.volumeInner*self.density),self.specificHeat ]
+
+    def initLambda(self):
+        lambdas=[]
+        solved=False
+        lastLambda=0
+        a=0.16                        #a is length of flask
+        
+        for i in range(31):         #can do more for both!
+            step=(((2*i-1)*math.pi/(2*a))-((i-1)*math.pi/(a)))/100                  
+            lastLambda=((i-1)*math.pi/(a))+step
+            while(not solved):
+                if(math.tan(lastLambda*a)-self.convectionCoeff/(self.specificHeat*lastLambda)<0.01):
+                    lambdas.append(lastLambda)
+                if(lastLambda>((2*i-1)*math.pi/(2*a))):
+                    
+                    break
+                lastLambda=lastLambda+step
+        
+        return lambdas
+
+
+
+
 
 
     def paperModel(self,temps):
         pass
+
+
+        # for i in range(31):
+
+
+
+
+
+    
     def ownModel(self,temps):
         pass
+
+
+
     def simplifiedPaper(self,temps):
         newTemp=(temps[0]-temps[1])*math.exp(((-temps[2]*temps[3]/(100**3))/(temps[4]*temps[5]))*self.timeInterval)+temps[1]
         print (newTemp)
